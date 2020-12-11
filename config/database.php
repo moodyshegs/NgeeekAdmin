69d0d5a03ec0cdfc05bf1f9882ec5c1aba91823b
moodyshegs <61108798+moodyshegs@users.noreply.github.com>
2020-12-11 19:31:58 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('CLEARDB_DATABASE_URL'),
+            'host' => env('DB_HOST', 'us-cdbr-east-02.cleardb.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_0f8d039da71c838'),
+            'username' => env('DB_USERNAME', 'b58d87c7abd739'),
+            'password' => env('DB_PASSWORD', 'f1fce342'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
